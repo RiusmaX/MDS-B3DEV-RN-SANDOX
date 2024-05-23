c7ae0a1d0171c68d74809caac1562baddf2bfffd
Marius <marius.sergent@gmail.com>
2024-05-23 16:09:41 +0200

    mise en forme resultats

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,35 @@
-import { ScrollView, Text } from 'react-native'
+import { FlatList, ScrollView, Text, View } from 'react-native'
+import styles from '../styles/ResultStyle'
+
+function ResultItem ({ item }) {
+  let color = 'black'
+  if (item.value > 0.95) {
+    color = 'green'
+  } else if (item.value > 0.90 && item.value <= 0.95) {
+    color = 'orange'
+  } else if (item.value <= 0.90) {
+    color = 'red'
+  }
+  return (
+    <View style={styles.listItem}>
+      <Text style={{ color, fontWeight: 'bold' }}>{item.name}</Text>
+      <Text style={{ color, fontWeight: 'bold' }}>{(item.value * 100).toFixed(2)}%</Text>
+    </View>
+  )
+}
 
 function Result ({ result }) {
+  const concepts = result?.outputs[0]?.data?.concepts
   return (
-    <ScrollView style={{ flex: 1 }}>
-
-      <Text>{JSON.stringify(result, null, 2)}</Text>
+    <ScrollView style={styles.container}>
+      <FlatList
+        data={concepts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ResultItem item={item} />
+        )}
+      />
+      {/* <Text>{JSON.stringify(result, null, 2)}</Text> */}
     </ScrollView>
   )
 }
---
+++ src/styles/ResultStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%'
+  },
+  listItem: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 50,
+    paddingVertical: 15,
+    borderBottomWidth: 1
+  }
+})
