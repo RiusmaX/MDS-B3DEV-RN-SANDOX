03148a6cecf428f9f45503529a62e9e6284847f6
Marius <marius.sergent@gmail.com>
2024-06-06 16:02:05 +0200

    add french langage

--- src/services/ClarifaiApi.js
+++ src/services/ClarifaiApi.js
@@ -38,7 +38,7 @@ const identifyImageWithLlava = async (imageBase64) => {
               base64: imageBase64
             },
             text: {
-              raw: "For an e-commerce site, I'd like you to describe the product in this image. Your tone should be engaging and commercial. You must respond with an JSON object directly, containing: the product title named “title”, the product description named “description” and 5 keywords associated with the product in an array named “tags”. I need to parse it directly with code."
+              raw: "For an e-commerce site, I'd like you to describe the product in this image. Your tone should be engaging and commercial. You must respond with an JSON object directly, containing: the product title named “title”, the product description named “description” and 5 keywords associated with the product in an array named “tags”. I need to parse it directly with code. You need to answer in French."
             }
           }
         }
